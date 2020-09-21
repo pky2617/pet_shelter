21204d22a06e5c17f88471299e0af95c8ba0109c
pky2617 <65819587+pky2617@users.noreply.github.com>
2020-09-21 11:03:26 -0600

    updated / path to getPets page

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { petCreatePage, petsPage, petDetailsPage } from '../../controllers/petsController';
 const indexRouter = express.Router();
 
-//indexRouter.get('/', indexPage);
+indexRouter.get('/', petsPage);
 indexRouter.get('/pets', petsPage);
 indexRouter.post('/pet', petCreatePage);
 indexRouter.get('/pets/:petId', petDetailsPage);
