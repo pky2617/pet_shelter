a544e2fa020f02315860cbdb7f3ee930464ab621
pky2617 <65819587+pky2617@users.noreply.github.com>
2020-09-21 23:36:08 -0600

    updated CORS settings

--- src/app.js
+++ src/app.js
@@ -1,13 +1,24 @@
-import logger from 'morgan';
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import indexRouter from './routes/index';
+import logger from "morgan";
+import express from "express";
+import cookieParser from "cookie-parser";
+import indexRouter from "./routes/index";
+import cors from "cors";
+//const cors = require('cors');
 const app = express();
 
-app.use(logger('dev'));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use('/', indexRouter);
+app.use("/", indexRouter);
+app.use(cors({ origin: true }));
 
 export default app;
