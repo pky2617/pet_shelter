879222e784419f1114657894347bdcabd5a85d85
pky2617 <65819587+pky2617@users.noreply.github.com>
2020-09-23 15:31:17 -0600

    updated issue for conversion from API res to Json object

--- controllers/petsController.js
+++ controllers/petsController.js
@@ -1,40 +1,39 @@
-const models = require('../src/models/pets');
+const models = require("../src/models/pets");
 exports.petCreatePage = async (req, res) => {
-    try {
-    const newPet = new models.Pet(req.body)
-    await newPet.save()
-    res.json({ pet: newPet }) // Returns the new user that is created in the database
-    } catch(error) {
-    console.error(error)
-    res.json(error)
-    }
-    };
+  try {
+    const newPet = new models.Pet(req.body);
+    await newPet.save();
+    res.json({ pet: newPet }); // Returns the new user that is created in the database
+  } catch (error) {
+    console.error(error);
+    res.json(error);
+  }
+};
 
 exports.petsPage = async (req, res) => {
-     try {
-        const pets = await models.Pet.findAll({
-            
-        }
-        )
-        res.json({ pets })
-    } catch (error) {
-        console.error(error)
-        res.json(error)
-    }
+  try {
+    const pets = await models.Pet.findAll({});
+    res.setHeader("Content-Type", "application/json");
+    res.json(pets);
+  } catch (error) {
+    console.error(error);
+    res.json(error);
+  }
 };
 exports.petDetailsPage = async (req, res) => {
-    const petId = req.params.petId;
-    console.log(`Id from param is ${petId}`);
-    try {
-        const pet = await models.Pet.findAll({
-            where: {
-                id: petId
-            }
-        }
-        )
-        res.json({ pet })
-    } catch (error) {
-        console.error(error)
-        res.json(error)
-    }
+  const petId = req.params.petId;
+  console.log(`Id from param is ${petId}`);
+  try {
+    const pet = await models.Pet.findAll({
+      where: {
+        id: petId,
+      },
+    });
+    res.setHeader("Content-Type", "application/json");
+    res.json(pet);
+    console.log("res ", res);
+  } catch (error) {
+    console.error(error);
+    res.json(error);
+  }
 };
