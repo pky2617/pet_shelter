895f7568ab4731c55b47f5bd853b77a76e018bd0
pky2617 <65819587+pky2617@users.noreply.github.com>
2020-09-23 23:12:18 -0600

    updated location key in the DB table

--- src/models/pets.js
+++ src/models/pets.js
@@ -1,28 +1,27 @@
-let dotenv = require('dotenv');
-let { connectionString } = require('../config');
-let { Sequelize } = require('sequelize');
+let dotenv = require("dotenv");
+let { connectionString } = require("../config");
+let { Sequelize } = require("sequelize");
 dotenv.config();
 
 const sequelize = new Sequelize(connectionString);
 sequelize
-.authenticate()
-.then(() => {
-console.log('Connection has been established successfully.');
-})
-.catch(err => {
-console.error('Unable to connect to the database:', err);
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
+
+exports.Pet = sequelize.define("pets", {
+  // attributes
+  name: { type: Sequelize.STRING, allowNull: false, unique: "compositeIndex" },
+  type: { type: Sequelize.STRING, allowNull: false },
+  breed: { type: Sequelize.STRING, allowNull: false, unique: "compositeIndex" },
+  location: { type: Sequelize.STRING, allowNull: true },
+  latitude: { type: Sequelize.FLOAT, allowNull: true },
+  longitude: { type: Sequelize.FLOAT, allowNull: true },
+  location_key: { type: Sequelize.FLOAT, allowNull: false },
 });
 
-exports.Pet = sequelize.define('pets', {
-    // attributes
-    name: { type: Sequelize.STRING, allowNull: false, unique: 'compositeIndex'},
-    type: { type: Sequelize.STRING, allowNull: false},
-    breed: { type: Sequelize.STRING, allowNull: false, unique: 'compositeIndex'},
-    location: { type: Sequelize.STRING, allowNull: true},
-    latitude: { type: Sequelize.FLOAT, allowNull: true},
-    longitude: { type: Sequelize.FLOAT, allowNull: true}
-}
-    );
-
-    //Pet.sync({ force: false }) ;
-
+//Pet.sync({ force: false }) ;
